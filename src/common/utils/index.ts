f3f3252ba87173f997fb0dabd9b6f4e4699e77c9
KirillovaD <sdaria@inbox.ru>
2023-04-26 22:42:22 +0300

    20.11 add pritter

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -1,3 +1,1 @@
-export {handleServerAppError} from 'common/utils/handle-server-app.error'
-export {handleServerNetworkError} from 'common/utils/handle-server-network.error'
 export {createAppAsyncThunk} from 'common/utils/create-app-async.thunk'
